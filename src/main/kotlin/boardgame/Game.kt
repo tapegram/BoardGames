9738f62699b1bb76648a81bc8bf329920e5fc452
Tavish Pegram <tavish@onepeloton.com>
2019-12-13 22:03:37 -0500

    wip, testing out different objects and how to approach testing

--- src/main/kotlin/boardgame/Game.kt
+++ src/main/kotlin/boardgame/Game.kt
@@ -1,5 +1,53 @@
 package boardgame
 
-data class Game(val id: String) {
-    fun say_hello(): String = "Hello, World"
+data class Player(
+    val name: String
+)
+
+data class BoardGame(
+    val board: Board,
+    val players: Pair<Player, Player>,
+    val currentTurn: Player,
+    val winConditions: List<WinCondition>
+) {
+    fun makeMove(move: Move): BoardGame {
+        return this.copy(
+            board=board.makeMove(move)
+        )
+    }
 }
+
+interface WinCondition
+
+sealed class Space {
+    data class Occupied(val piece: Piece) : Space()
+    object Empty : Space()
+}
+
+data class Board(
+    val board: List<List<Space>>
+) {
+    fun makeMove(move: Move): Board {
+        if (!can_make_move(move)) {
+            return this
+        }
+        val space = board[move.x][move.y]
+        return when (space) {
+            is Space.Occupied -> this
+            is Space.Empty -> this
+        }
+    }
+
+    private fun can_make_move(move: Move): Boolean = true
+
+}
+
+data class Piece(
+    val moves: List<Move>
+)
+
+data class Move(
+    val x: Int,
+    val y: Int
+)
+
